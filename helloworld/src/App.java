4dd9fedf05c32b8f21f594b13c4ba009b2aeb0ee
osuke.mori <osuke.mori@avantorp.com>
2025-06-11 15:51:54 +0900

    2回目です

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("こちらがmainです");
+        System.out.println("こちらがmainです2回目");
     }
 }
